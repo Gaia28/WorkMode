ab298217546f9eba59a7bfea0a10eaf6c2d36fee
DevGaia <146499021+Gaia28@users.noreply.github.com>
2026-01-09 09:35:31 -0300

    Refactor devMode.sh for better environment management
    Refactor devMode.sh to encapsulate web environment startup in a function and add a stop option.

--- devMode.sh
+++ devMode.sh
@@ -1,19 +1,38 @@
 #!/bin/bash
 
-code > /dev/null 2>&1 &
-
-firefox > /dev/null 2>&1 &
-
-flatpak run com.spotify.Client > /dev/null 2>&1 &
-
-sudo /opt/lampp/lampp start > /dev/null 2>&1 &
-
-echo "
+MESSAGE="
 ██╗      ███████╗███████╗████████╗███████╗      ██████╗ ██████╗ ██████╗ ███████╗
 ██║      ██╔════╝██╔════╝╚══██╔══╝██╔════╝     ██╔════╝██╔═══██╗██╔══██╗██╔════╝
 ██║      █████╗  ███████╗   ██║   ███████╗     ██║     ██║   ██║██║  ██║█████╗  
 ██║      ██╔══╝  ╚════██║   ██║   ╚════██║     ██║     ██║   ██║██║  ██║██╔══╝  
 ███████╗ ███████╗███████║   ██║   ███████║     ╚██████╗╚██████╔╝██████╔╝███████╗
 ╚══════╝ ╚══════╝╚══════╝   ╚═╝   ╚══════╝      ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝
-                              .
 "
+FINISH_MESSAGE="
+   _________
+  |         |
+  | dont    |
+  | kill me |
+  |_________|
+      ||
+      ||
+"
+start_web_enviroment() {
+   code > /dev/null 2>&1 &
+   firefox > /dev/null 2>&1 &
+   sudo /opt/lampp/lampp start > /dev/null 2>&1 &
+ 
+}
+
+if [ "$1" == "-s" ]; then
+   sudo /opt/lampp/lampp stop > /dev/null 2>&1
+   pkill firefox
+   pkill code
+   echo "$FINISH_MESSAGE"
+
+else 
+   start_web_enviroment
+   echo "$MESSAGE"
+
+fi                    
+
